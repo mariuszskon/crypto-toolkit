9fdc866a56128a23237544cd4ebc288603278d21
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-09-20 19:23:03 +1000

    Added vigenere cipher code

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-var javascriptsAll = ['header.js', 'data/alphabet.js', 'ciphers/caesar.js'];
+var javascriptsAll = ['header.js', 'data/alphabet.js', 'ciphers/caesar.js', 'ciphers/vigenere.js'];
 var javascriptsBrowser = ['ui/toolbar.js', 'ui/pages.js'];
 
 gulp.task('html', function() {
--- other/js/crypto-toolkit.js
+++ other/js/crypto-toolkit.js
@@ -81,6 +81,56 @@ cipher.caesar = function(mode, input, key) {
     
 };
 
+// vigenere.js
+
+cipher.vigenereGetKeyLetterAlphabetPosition = function(number, key) { // take number as the position of the input letter, so that we can match it with a key letter
+    var keyLetterPosition = number % key.length; // % uses modular arithmetic to "loop around", matching a number to the key letter position
+    
+    var keyLetterAtPosition = key.charAt(keyLetterPosition);
+    
+    return data.alphabet.indexOf(keyLetterAtPosition);
+};
+
+cipher.vigenere = function(mode, input, key) {
+    var finaloutput = "";
+    
+    for (var i = 0; i < input.length; i++) {
+        
+        if (input.charAt(i) == "\n") {
+            
+            continue; // it's a newline: we don't need to keep going - go straight to the next letter
+            
+        }
+        
+        var inputLetterPosition = data.alphabet.indexOf(input.charAt(i));
+        
+        var finalLetterPosition = "";
+        
+        if (mode === true) { // encryption mode
+            
+            finalLetterPosition = (inputLetterPosition + cipher.vigenereGetKeyLetterAlphabetPosition(i, key)) % data.alphabet.length; // add the position of the original letter with the alphabet position of the corresponding key letter, and wrap around the alphabet length
+            
+        } else if (mode === false) { // decryption mode
+            
+            var possibleLetterPosition = (inputLetterPosition - cipher.vigenereGetKeyLetterAlphabetPosition(i, key));
+            
+            if (possibleLetterPosition < 0) {
+                finalLetterPosition = possibleLetterPosition + data.alphabet.length; // if the number is negative, "loop around" by adding the length of the alphabet to it
+            } else {
+                finalLetterPosition = possibleLetterPosition;
+            }
+            
+        }
+        
+        finaloutput += data.alphabet[finalLetterPosition]; // get the actual letter and add it to the finaloutput
+        
+    }
+    
+    return finaloutput;
+    
+};
+
+
 // toolbar.js
 
 var mainToolbarButtons = document.getElementById("main-ul").getElementsByTagName("a");
