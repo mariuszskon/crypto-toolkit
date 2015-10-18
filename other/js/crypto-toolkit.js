39d8956b4a3864d73169e2784e8989814909889f
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-10-18 19:51:17 +1100

    Latest build

--- other/js/crypto-toolkit.js
+++ other/js/crypto-toolkit.js
@@ -460,3 +460,24 @@ function rsadomcrypt(e) {
 
 gid("rsa-encrypt").addEventListener("click", rsadomcrypt);
 gid("rsa-decrypt").addEventListener("click", rsadomcrypt);
+
+// dh-dom.js
+
+gid("dh-gen-pub").addEventListener("click", function() {
+    var bits = document.querySelector("#dh-public-size input:checked").value;
+    var privkey = gid("dh-priv").value;
+    
+    var result = modern.dhGenPub(data.rfc3526["_" + bits], privkey);
+    
+    gid("dh-pub-key").innerHTML = result.toString();
+});
+
+gid("dh-final").addEventListener("click", function() {
+    var bits = document.querySelector("#dh-public-size input:checked").value;
+    var privkey = gid("dh-priv").value;
+    var theirpub = gid("dh-their-pub").value;
+    
+    var result = modern.dhGenSharedSec(data.rfc3526["_" + bits], theirpub, privkey);
+    
+    gid("dh-final-key").innerHTML = result.toString();
+});
