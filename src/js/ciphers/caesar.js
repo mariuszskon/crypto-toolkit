e0f83902eb344613b02d82de63fbd257d741bbb4
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2015-10-24 15:31:07 +1100

    Small Caesar cipher code cleanup

--- src/js/ciphers/caesar.js
+++ src/js/ciphers/caesar.js
@@ -5,7 +5,7 @@ cipher.caesar = function(mode, input, key) {
     
     var outputtext = "";
     
-    var cinput = input.replace(/\s/g, ""); // strip out all spaces
+    var cinput = input.replace(/\s/g, "").toLowerCase(); // strip out all spaces and change case to lowercase
     
     if (key < 1 || key > (data.alphabet.length - 1)) {
         return null; // end the function
@@ -17,7 +17,7 @@ cipher.caesar = function(mode, input, key) {
         var l;
         
         if (mode === true) { // encryption mode
-            position = data.alphabet.indexOf(cinput.charAt(i).toLowerCase()); // normalize input
+            position = data.alphabet.indexOf(cinput.charAt(i));
             
             l = position + key;
             
@@ -26,7 +26,7 @@ cipher.caesar = function(mode, input, key) {
             outputtext += data.upperbet[l]; // put the outputtext letter in the ciphertext (UPPERCASE)
             
         } else if (mode === false) { // decryption mode
-            position = data.alphabet.indexOf(cinput.charAt(i).toLowerCase()); // normalize input
+            position = data.alphabet.indexOf(cinput.charAt(i));
             
             l = position - key;
             
